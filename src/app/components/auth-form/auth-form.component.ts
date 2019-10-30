3079a76f26f9ebae179ee71e4b2f988006b7c4c5
Tauqeer Ahmed <tauqeer.shakir@yahoo.com>
2019-10-30 12:40:02 +0500

    Common component created for login and signup with form validation

--- src/app/components/auth-form/auth-form.component.ts
+++ src/app/components/auth-form/auth-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-form',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-form.component.scss']
 })
 export class AuthFormComponent implements OnInit {
-
-  constructor() { }
+  authForm: FormGroup;
+  submitted = false;
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
   }
 
+  // get form control email
+  get email() {
+    return this.authForm.controls.email;
+  }
+
+  // get form control password
+  get password() {
+    return this.authForm.controls.password;
+  }
+
+  // Form submit method
+  onSubmit() {
+    this.submitted = true;
+    if (!this.authForm.valid) {
+      return;
+    }
+    const { email, password } = this.authForm.value;
+    console.log('email', email);
+    console.log('password', password);
+  }
 }
--- src/app/components/auth-form/auth-form.module.ts
+++ src/app/components/auth-form/auth-form.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthFormComponent } from './auth-form.component';
 
 @NgModule({
   declarations: [AuthFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [AuthFormComponent]
 })
 export class AuthFormModule {}
