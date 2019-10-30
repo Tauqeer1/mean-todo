4feff97799d4cd3dfad6d5cc48f2b4b028a30cb7
Tauqeer Ahmed <tauqeer.shakir@yahoo.com>
2019-10-30 14:21:32 +0500

    todo adding functionality added

--- src/app/containers/dashboard/dashboard.component.ts
+++ src/app/containers/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ToastrService } from 'ngx-toastr';
+
+import { TodoService } from '../../services/todo/todo.service';
+
 import { TodoItem } from '../../models/todo';
 
 @Component({
@@ -8,11 +12,32 @@ import { TodoItem } from '../../models/todo';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  todos: Array<Partial<TodoItem>>;
+  constructor(
+    private toastrService: ToastrService,
+    private todoService: TodoService
+  ) {}
 
   ngOnInit() {}
 
   addTodo(todoObj: TodoItem) {
-    console.log('todoObj', todoObj);
+    this.todoService.addTodo(todoObj).subscribe(
+      todo => {
+        this.toastrService.success('Todo added successfully', 'Added');
+        this.todos.push(todo);
+      },
+      errorObj => {
+        if (
+          errorObj &&
+          errorObj.error &&
+          errorObj.error.error &&
+          errorObj.error.error.msg
+        ) {
+          this.toastrService.error(errorObj.error.error.msg, 'Error!');
+        } else {
+          this.toastrService.error('Something went wrong!');
+        }
+      }
+    );
   }
 }
---
+++ src/app/services/todo/todo.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+import { environment } from '../../../environments/environment';
+
+import { TodoItem } from '../../models/todo';
+import { ResponseObj } from '../../models/res';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  readonly BASE_URL: string = environment.baseURL;
+  constructor(private httpClient: HttpClient) {}
+
+  addTodo(todo: TodoItem) {
+    return this.httpClient
+      .post<ResponseObj>(`${this.BASE_URL}/api/todo`, todo)
+      .pipe(
+        map(res => {
+          if (res.success) {
+            return res.data;
+          }
+        })
+      );
+  }
+}
