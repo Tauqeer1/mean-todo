fed703fffd03df3ef9d230ac6583e6542a08f500
Tauqeer Ahmed <tauqeer.shakir@yahoo.com>
2019-10-30 13:14:00 +0500

    register form ui added

--- src/app/containers/register/register.module.ts
+++ src/app/containers/register/register.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthFormModule } from '../../components/auth-form/auth-form.module';
+
 import { RegisterComponent } from './register.component';
 
 // Register route
@@ -14,7 +16,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), AuthFormModule],
   exports: [RegisterComponent]
 })
 export class RegisterModule {}
