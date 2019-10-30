d1c90363801d9fbb91300e14230c1718b833de8c
Tauqeer Ahmed <tauqeer.shakir@yahoo.com>
2019-10-30 12:58:30 +0500

    Login page form added with content projection

--- src/app/containers/login/login.module.ts
+++ src/app/containers/login/login.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthFormModule } from '../../components/auth-form/auth-form.module';
+
 import { LoginComponent } from './login.component';
 
 // Login route
@@ -14,7 +16,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), AuthFormModule],
   exports: [LoginComponent]
 })
 export class LoginModule {}
